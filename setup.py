be196064379c41ceb45ff802bdece8d546d1cbe8
giuserizz <grizzi@leggedrobotics.com>
2020-06-04 18:06:10 +0200

    add dependencies

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
 import setuptools
 
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
 setuptools.setup(name='robot_control',
                  version='0.0.1',
+                 long_description=long_description,
                  packages=['robot_control',
                            'robot_control.controllers',
                            'robot_control.controllers.implementation',
@@ -11,4 +15,5 @@ setuptools.setup(name='robot_control',
                            'robot_control.simulation',
                            'robot_control.simulation.sensors'],
                  package_dir={'': 'src'},
-                 python_requires='>=3.6')
+                 python_requires='>=3.6',
+                 install_requires=['numpy', 'scipy', 'pybullet'])
