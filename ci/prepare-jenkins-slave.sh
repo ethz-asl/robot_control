b769bc1a6dcec8820910f586b13a23099fb0284c
giuserizz <grizzi@leggedrobotics.com>
2020-06-10 14:37:58 +0200

    minor change

--- ci/prepare-jenkins-slave.sh
+++ ci/prepare-jenkins-slave.sh
@@ -2,12 +2,11 @@
 echo "Running the prepare script for robot_control.";
 catkin config --cmake-args -DCMAKE_CXX_STANDARD=17
 
-
 echo "Installing pinocchio"
 sudo sh -c "echo 'deb [arch=amd64] http://robotpkg.openrobots.org/packages/debian/pub bionic robotpkg' >> /etc/apt/sources.list.d/robotpkg.list"
-curl http://robotpkg.openrobots.org/packages/debian/robotpkg.key | sudo apt-key add -
-sudo apt-get update
-sudo apt install robotpkg-py27-pinocchio
+curl http://robotpkg.openrobots.org/packages/debian/robotpkg.key | sudo apt-key add -y -
+sudo apt-get update -y
+sudo apt install -y robotpkg-py27-pinocchio
 
 export PATH=/opt/openrobots/bin:$PATH
 export PKG_CONFIG_PATH=/opt/openrobots/lib/pkgconfig:$PKG_CONFIG_PATH
