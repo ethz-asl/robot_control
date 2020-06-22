8a7752252a66fd309719a0276a81d33ade3ebea3
giuserizz <grizzi@leggedrobotics.com>
2020-06-22 17:13:15 +0200

    lets try this way

--- ci/prepare-jenkins-slave.sh
+++ ci/prepare-jenkins-slave.sh
@@ -7,14 +7,10 @@ sudo apt-get install -y ros-melodic-libfranka ros-melodic-franka-ros ros-melodic
 
 echo "Installing pinocchio"
 sudo apt-get install -y robotpkg-py27-pinocchio
-ls /opt/openrobots/bin
-export PATH=/opt/openrobots/bin:$PATH
-export PKG_CONFIG_PATH=/opt/openrobots/lib/pkgconfig:$PKG_CONFIG_PATH
-export LD_LIBRARY_PATH=/opt/openrobots/lib:$LD_LIBRARY_PATH
-export PYTHONPATH=/opt/openrobots/lib/python2.7/site-packages:$PYTHONPATH
-export CMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH:/opt/openrobots
-printenv PATH
-ls /opt/openrobots/lib
+$PATH=/opt/openrobots/bin:$PATH
+$PKG_CONFIG_PATH=/opt/openrobots/lib/pkgconfig:$PKG_CONFIG_PATH
+$LD_LIBRARY_PATH=/opt/openrobots/lib:$LD_LIBRARY_PATH
+$PYTHONPATH=/opt/openrobots/lib/python2.7/site-packages:$PYTHONPATH
 
 #cd robot_control/submodules/pinocchio
 #git checkout v2.4.6
