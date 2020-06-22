afcd804e4beed0e237215d067b54e7b38c06f415
giuserizz <grizzi@leggedrobotics.com>
2020-06-22 18:52:01 +0200

    not using export

--- ci/source_env.sh
+++ ci/source_env.sh
@@ -1,5 +1,5 @@
 echo "Expanding env variables to find pinocchio"
-export PATH=/opt/openrobots/bin:$PATH
-export PKG_CONFIG_PATH=/opt/openrobots/lib/pkgconfig:$PKG_CONFIG_PATH
-export LD_LIBRARY_PATH=/opt/openrobots/lib:$LD_LIBRARY_PATH
-export PYTHONPATH=/opt/openrobots/lib/python2.7/site-packages:$PYTHONPATH
+PATH=/opt/openrobots/bin:$PATH
+PKG_CONFIG_PATH=/opt/openrobots/lib/pkgconfig:$PKG_CONFIG_PATH
+LD_LIBRARY_PATH=/opt/openrobots/lib:$LD_LIBRARY_PATH
+PYTHONPATH=/opt/openrobots/lib/python2.7/site-packages:$PYTHONPATH
