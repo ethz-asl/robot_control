5db2e1b5100f37e9d08f5da07196b1ae18dbdd73
giuserizz <grizzi@leggedrobotics.com>
2020-06-22 16:39:57 +0200

    changing again the prepare script

--- ci/prepare-jenkins-slave.sh
+++ ci/prepare-jenkins-slave.sh
@@ -2,19 +2,20 @@
 echo "Running the prepare script for robot_control.";
 catkin config --cmake-args -DCMAKE_CXX_STANDARD=17
 
-sudo apt install ros-melodic-libfranka ros-melodic-franka-ros
+sudo apt-get install -y ros-melodic-libfranka ros-melodic-franka-ros
 
 
 echo "Installing pinocchio"
-cd robot_control/submodules/pinocchio
-git checkout v2.4.6
-mkdir build && cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr/local
-make -j4
-make install 
+sudo apt install robotpkg-py27-pinocchio
+export PATH=/opt/openrobots/bin:$PATH
+export PKG_CONFIG_PATH=/opt/openrobots/lib/pkgconfig:$PKG_CONFIG_PATH
+export LD_LIBRARY_PATH=/opt/openrobots/lib:$LD_LIBRARY_PATH
+export PYTHONPATH=/opt/openrobots/lib/python2.7/site-packages:$PYTHONPATH
 
-export PATH=/usr/local/bin:$PATH
-export PKG_CONFIG_PATH =/usr/local/lib/pkgconfig:$PKG_CONFIG_PATH
-export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
-export PYTHONPATH =/usr/local/lib/python2.7/site-packages:$PYTHONPATH
+#cd robot_control/submodules/pinocchio
+#git checkout v2.4.6
+#mkdir build && cd build
+#cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr/local
+#make -j4
+#make install 
 
