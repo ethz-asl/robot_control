bb0f132d8a7a4f78e3c134f349de558e33583299
giuserizz <grizzi@leggedrobotics.com>
2020-06-22 17:45:56 +0200

    script for setting env variables

---
+++ ci/source_env.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -e
+
+echo "Expanding env variables to find pinocchio"
+export PATH=/opt/openrobots/bin:$PATH
+export PKG_CONFIG_PATH=/opt/openrobots/lib/pkgconfig:$PKG_CONFIG_PATH
+export LD_LIBRARY_PATH=/opt/openrobots/lib:$LD_LIBRARY_PATH
+export PYTHONPATH=/opt/openrobots/lib/python2.7/site-packages:$PYTHONPATH
